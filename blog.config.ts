e8ac42bcdb979215135957e93b1c12e801312a3f
Alex Castro <git@alexcastro.dev>
2025-11-22 15:16:52 -0300

    Updates site URL and plugin source branch
    Switches to the correct production site address for improved branding
    and changes the Gatsby plugin source to track the main branch for
    latest updates and bug fixes.

--- blog.config.ts
+++ blog.config.ts
@@ -16,7 +16,7 @@ export default {
     twitterUsername: `<your-tt-username>`,
     description: `Obsidian is a blog and template built with Gatsby and GitHub, free and open-source.`,
     image: `https://user-images.githubusercontent.com/51419598/240399860-a9fb362c-28a0-4838-b8d5-8332c2feecb5.png`,
-    siteUrl: `https://obsidian.alexisa.dev`,
+    siteUrl: `https://alexcastro.dev`,
   },
   socialMedia: socialMedia,
   postsPerPage: 10,
